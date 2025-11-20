e15f1d8ed19e27492f4c79813ec0680ada882d7c
OlegGulevskyy <oleggulevskyy@gmail.com>
2025-11-20 15:23:15 +0100

    init

---
+++ src/layouts/main.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="root">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.root {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
